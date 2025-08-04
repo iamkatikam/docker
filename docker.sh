deb39567caba55e58d2e0e310929b8599de0ca46
Ramesh Katikam <katikam.rams@gmail.com>
2025-08-04 12:03:47 +0530

    docker user input file for helm

--- docker.sh
+++ docker.sh
@@ -14,12 +14,14 @@ lvextend -L +10G /dev/RootVG/varVol
 xfs_growfs /
 xfs_growfs /var
 
+#install eksctl
 ARCH=amd64
 PLATFORM=$(uname -s)_$ARCH
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 install -m 0755 /tmp/eksctl /usr/local/bin && rm /tmp/eksctl
 
+#install kubectl
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mv kubectl /usr/local/bin/kubectl
@@ -27,5 +29,11 @@ mv kubectl /usr/local/bin/kubectl
 eksctl version
 kubectl version
 
+#install kubens
 git clone https://github.com/ahmetb/kubectx /opt/kubectx
 ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
+#install helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+ ./get_helm.sh
